6888aeb39c1a58d994c85e2456ced4c7fd5f9cc9
Lavosami <vap210504@gmail.com>
2023-06-07 01:53:11 +0300

    new class for telegram bot

---
+++ src/main/java/com/lavosami/Bot.java
@@ -0,0 +1,10 @@
+package com.lavosami;
+
+import com.pengrad.telegrambot.TelegramBot;
+
+public class Bot {
+    String token = "6057937237:AAHA_o1N6t8MtHj5GQr-tR--88C0iYqAZ_Q";
+
+    TelegramBot
+
+}
