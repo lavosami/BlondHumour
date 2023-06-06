21ee258508df382062939a1e9dd22bfb30585818
Lavosami <vap210504@gmail.com>
2023-06-07 01:43:13 +0300

    added function that returns random meme about blonds

--- src/main/java/com/lavosami/Main.java
+++ src/main/java/com/lavosami/Main.java
@@ -2,18 +2,37 @@ package com.lavosami;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Document document = Jsoup.connect("https://anekdotovstreet.com/blondinki/?ysclid=likoo2benz6729960").get();
-        Elements elements = document.select("div.anekdot-text > p");
+    static Document document;
 
-        for (Element element : elements) {
-            System.out.println(element);
+    static {
+        try {
+            document = Jsoup.connect("https://anekdotovstreet.com/blondinki/?ysclid=likoo2benz6729960").get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    static Elements elements = document.select("div.anekdot-text > p");
+
+    static public String randomMeme() {
+        Random random = new Random();
+
+        String str = String.valueOf(elements.get(random.nextInt(elements.size())));
+
+        str = str.replace("<p>", "");
+        str = str.replace("<br>", "");
+        str = str.replace("</p>", "");
+
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(randomMeme());
     }
 }
