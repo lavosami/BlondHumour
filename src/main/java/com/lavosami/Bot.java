d8d2c8bf2cb25237115cd36fa722389c13494467
Lavosami <vap210504@gmail.com>
2023-06-07 12:02:30 +0300

    added dependencies for telegram bot

--- src/main/java/com/lavosami/Bot.java
+++ src/main/java/com/lavosami/Bot.java
@@ -1,5 +1,25 @@
 package com.lavosami;
 
-public class Bot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
