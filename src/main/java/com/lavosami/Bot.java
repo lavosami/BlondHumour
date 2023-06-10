71fd5578bfe543814a15b6d7109b545f0ed0e343
Lavosami <vap210504@gmail.com>
2023-06-10 05:39:01 +0300

    now bot takes chat id from message and set random joke to user

--- src/main/java/com/lavosami/Bot.java
+++ src/main/java/com/lavosami/Bot.java
@@ -1,34 +1,42 @@
 package com.lavosami;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Bot extends TelegramLongPollingBot {
+
     @Override
     public void onUpdateReceived(Update update) {
-        // We check if the update has a message and the message has text
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
-            message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(update.getMessage().getText());
-
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                try {
+                    execute(SendMessage.builder().chatId(message.getChatId()).text(Jokes.randomJoke()).build());
+                } catch (TelegramApiException e) {
+                    throw new RuntimeException(e);
+                }
             }
         }
     }
 
     @Override
     public String getBotUsername() {
-        return "BlondJokes";
+        return "@blond_jokes_bot";
     }
 
     @Override
     public String getBotToken() {
         return "6057937237:AAHA_o1N6t8MtHj5GQr-tR--88C0iYqAZ_Q";
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        Bot bot = new Bot();
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(bot);
     }
 }
